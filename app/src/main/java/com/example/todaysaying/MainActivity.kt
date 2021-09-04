13ae74201f0690e765f82ebf97c3ce9305c75df4
eigen98 <terrantrace@gmail.com>
2021-09-05 03:41:43 +0900

    remote config fetch

--- app/src/main/java/com/example/todaysaying/MainActivity.kt
+++ app/src/main/java/com/example/todaysaying/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.todaysaying
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +19,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initViews()
+
+        initData()
     }
 
     private fun initViews() {
+
+    }
+
+    private fun initData(){
+        //시간 단축시키기
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+                remoteConfigSettings {
+                    minimumFetchIntervalInSeconds = 0
+                }
+        )
+
+        //fetch자체가 서버와 통신이기에 비동기로 이루어짐. -> 리스너를 등록
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if(it.isSuccessful){//작업이 성공했다면
+                val quotes = parseQuotesJson(remoteConfig.getString("quotes"))//기본 Api를 사용해서 Json파싱
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuotesPager(quotes, isNameRevealed)
+
+            }
+        }
+    }
+
+    private fun parseQuotesJson(json : String): List<Quote>{
+        val jsonArray = JSONArray(json) //json String을 가지고 새로운 array를 만듬
+        var jsonList =  emptyList<JSONObject>() //Json Array를 Jason object List로 바꿀 변수를 만듬 //하나씩 가져와서 Json List에 추가
+        for(index in 0 until jsonArray.length()){
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let{
+                jsonList = jsonList + it //뒤에 하나씩 JsonObject가 붙음
+            }
+        }
+
+        return jsonList.map{
+            Quote(
+                    quote = it.getString("quote"),
+                    name = it.getString("name")
+            )
+        }
+    }
+
+    private fun displayQuotesPager(quotes :List<Quote> ,isNameRevealed : Boolean){
         viewPager.adapter = QuotesPagerAdapter(
-                listOf(Quote("나는 생각한다. 고로 나는 존재한다",
-                        "데카르트"))
+            quotes = quotes,
+            isNameRevealed = isNameRevealed
         ) //mainActivity에 있는 ViewPager와 연동 -> 뷰페이저어댑터를 설정
     }
+
 
 }
--- app/src/main/java/com/example/todaysaying/QuotesPagerAdapter.kt
+++ app/src/main/java/com/example/todaysaying/QuotesPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.todaysaying
 
+import android.text.BoringLayout
 import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
@@ -9,7 +10,8 @@ import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 
 class QuotesPagerAdapter(
-        private val quotes : List<Quote>    //아이템리스트
+        private val quotes : List<Quote>,    //아이템리스트
+        private val isNameRevealed : Boolean
 ) : RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {//뷰페이저2같은경우 리사이클러뷰를 기반
 //RecyclerView.Adapter<>상속받아야함 //제네릭 타입 안에 뷰 홀더를 넣어주어야함.
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){ //내부클래스로 뷰홀더 클래스 생성 :RecyclerView상속받아야함 //생성자에서 받은 itemView를 뷰홀더(부모클래스)에 넘겨줌
@@ -18,9 +20,17 @@ class QuotesPagerAdapter(
     private val quoteTextView : TextView = itemView.findViewById(R.id.quoteTextView)
     private val nameTextView : TextView = itemView.findViewById(R.id.nameTextView)
     //바인드메소드 구현
-        fun bind(quote : Quote){
+        fun bind(quote : Quote, isNameRevealed: Boolean){
             quoteTextView.text = quote.quote
-            nameTextView.text = quote.name
+            if(isNameRevealed){
+                nameTextView.text = quote.name
+                nameTextView.visibility = View.VISIBLE
+            }else{
+                nameTextView.visibility = View.GONE
+            }
+
+
+
         }
     }
 
@@ -33,7 +43,7 @@ class QuotesPagerAdapter(
             )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {     //어떤것을 바인드 할것인가
-        holder.bind(quotes[position])
+        holder.bind(quotes[position], isNameRevealed)
     }
 
     override fun getItemCount() = quotes.size
